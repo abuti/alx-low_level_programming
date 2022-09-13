2ecc57dbc87a806f8d9073a91bb92bf1f00158a7
abuti <besuneg2006@gmail.com>
2022-09-12 18:50:27 -0700

    0 corre

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  * 
  * Return: Always 0 (Success/correct
  */
-int main(0)
+int main(void)
 {
 	int n;
 	srand(time(0));
