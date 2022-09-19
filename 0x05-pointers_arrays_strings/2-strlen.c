dfa2d969c1149c5fddeaa6a8b4461ac32ba4f1a8
abuti <besuneg2006@gmail.com>
2022-09-19 13:57:01 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *strlen - calculates the size of a string
+ *@s: string the size to be calculated
+ *
+ *Return: size of the string/ integer
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
