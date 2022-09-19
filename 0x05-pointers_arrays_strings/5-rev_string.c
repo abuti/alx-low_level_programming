d17dc081e6d42f82e9b40c6eeddcfaf5cf183f89
abuti <besuneg2006@gmail.com>
2022-09-19 16:38:34 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,11 @@
 
 void rev_string(char *s)
 {
-	int i, tmp, len = strlen(s);
+	int i, tmp, len = _strlen(s);
+	
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = *(s + i);
 		*(s + i) = *(s + len - i - 1);
 		*(s + len - i - 1) = tmp;
 	}
-}
-
