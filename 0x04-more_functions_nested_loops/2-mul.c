a509b5dbc784289baf9ca0c8f5dfc659a59b14e7
abuti <besuneg2006@gmail.com>
2022-09-19 22:59:15 -0700

    task 2 correction

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * mul - gives the product of a and b
- * @a: first number
- * @b: second number
- *  
- * Return: returns the multiplication result
- */
+* mul - gives the product of a and b
+* @a: first number
+* @b: second number
+*
+* Return: returns the multiplication result
+*/
 
 int mul(int a, int b)
 {
