7354db5d01166e03b9f4910ee151c23a13ae7d95
abuti <besuneg2006@gmail.com>
2022-09-21 08:28:49 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strncat - concatinates two strings with the exception atmost n bytes from
+ *src string only src does not need to be null-terminated if it contains
+ *@dest: the string that the other string going to be added to
+ *@src: the string that will be appended
+ *n: maximum number of bytes
+ *
+ *Return: concatinated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		continue;
+	for (j = 0; (j != n) && (src[j] != '\0'); j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
+	return (dest);
+}
