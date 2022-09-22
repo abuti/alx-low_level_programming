f463eecaaf2426843bdc6c58c14cba4c91e93716
abuti <besuneg2006@gmail.com>
2022-09-22 00:08:33 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,8 +15,10 @@ char *_strcat(char *dest, char *src)
 
 	while (dest[i] != '\0')
 		i++;
-	for (j = 0; src[j] != '\0'; j++)
+
+	for (j = 0; src[j] != '\0'; ++j)
 		dest[i + j] = src[j];
+
 	dest[i + j] = '\0';
 	return (dest);
 }
