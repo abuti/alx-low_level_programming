3aef1eb29ef5e80f0ee12bf8050d1d5785c412bb
abuti <besuneg2006@gmail.com>
2022-11-18 05:14:25 -0800

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,23 +17,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 	unsigned int sz1, sz2, i, j;
 
-	sz1 = strlen(s1);
-	sz2 = strlen(s2);
-	ptr = malloc(sizeof(char) * (n + sz1));
-	if (ptr == NULL)
-		exit(1);
-	for (i = 0; i < sz1; i++)
-		ptr[i] = s1[i];
-	if (sz2 <= n)
+	if (s1 == NULL || s2 == NULL)
 	{
-		for (j = 0; j < sz2; j++)
-			ptr[sz1 + j] = s2[j];
+		ptr = " ";
+		return (ptr);
 	}
 	else
 	{
-		for (j = 0; j < n; j++)
-			ptr[sz1 + j] = s2[j];
+		sz1 = strlen(s1);
+		sz2 = strlen(s2);
+		ptr = malloc(sizeof(char) * (n + sz1));
+		if (ptr == NULL)
+			exit(1);
+		for (i = 0; i < sz1; i++)
+			ptr[i] = s1[i];
+		if (sz2 <= n)
+		{
+			for (j = 0; j < sz2; j++)
+				ptr[sz1 + j] = s2[j];
+		}
+		else
+		{
+			for (j = 0; j < n; j++)
+				ptr[sz1 + j] = s2[j];
+		}
+		ptr[sz1 + j + 2] = '\0';
+		return (ptr);
 	}
-	ptr[sz1 + j + 2] = '\0';
-	return (ptr);
 }
