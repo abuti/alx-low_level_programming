6396441c3409fff0104fc3fc0ebbf16520db3807
abuti <besuneg2006@gmail.com>
2022-11-21 07:14:15 -0800

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,9 +4,22 @@
 
 /**
  * _calloc - concatenates two strings
- * @nmemb:
- * @size:
- * Return:
+ * @nmemb: number of array elements
+ * @size: size of each element
+ * Return: a pointer to the allocated memory
  */
 
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr = NULL;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
+	return (ptr);
+}
