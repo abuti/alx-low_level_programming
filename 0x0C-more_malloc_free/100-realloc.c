099f1830bfaf66ef7861abe19d42801e19ee3e0f
abuti <besuneg2006@gmail.com>
2022-11-21 09:10:37 -0800

    4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	nptr = malloc(new_size);
 	if (nptr == NULL)
 		return (NULL);
-	for (i = 0; i < old_size || i < new_size; i++)
+	for (i = 0; i < (old_size || i < new_size); i++)
 		*(nptr + i) = tmp[i];
 	free(ptr);
 	return (nptr);
