7cc13fdd56130c73a1325c6bb3f94465e67735ae
abuti <besuneg2006@gmail.com>
2022-11-23 16:43:10 -0800

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a data stracture with different data types
+ * @name: name for the dog char*
+ * @age: age of the dog float
+ * @owner: owner of the dog char*
+ * Return: nothing.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
